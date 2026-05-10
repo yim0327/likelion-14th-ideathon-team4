7a777c8b4a127090832381bbbd44e6099cf301eb
yim0327 <yim020327@gmail.com>
2026-05-10 17:18:59 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/inuni/ideathon/IdeathonApplication.java
@@ -0,0 +1,13 @@
+package com.inuni.ideathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inuni/ideathon/IdeathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inuni.ideathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
